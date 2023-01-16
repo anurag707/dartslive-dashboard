bc2472a9d820241f2463a68b7b3cebfad49472e0
Anurag Gupta <gupta.anuragkly@gmail.com>
2023-01-17 00:09:33 +0545

    Report Generation Implemented for all test cases

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,48 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+// import allureWriter from "@shelex/cypress-allure-plugin/writer";
 
 module.exports = defineConfig({
+    // e2e: {
+    //     setupNodeEvents(on, config) {
+    //         allureWriter(on, config);
+    //         return config;
+            
+    //     }
+    // }
+    reporter: "cypress-mochawesome-reporter",
+    reporterOptions:{
+    charts: true,
+    reportPageTitle: "custom-title",
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  }, 
   e2e: {
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
+      allureWriter(on, config);
+      return config;
+      //screenshotOnRunFailure=true;
     },
-  },
+    
+  }
+    
+
 });
+
+
+// module.exports = defineConfig({
+//   reporter: 'cypress-mochawesome-reporter', //for html reports
+//   e2e: {
+//     setupNodeEvents(on, config) {
+//       // implement node event listeners here
+//       require('cypress-mochawesome-reporter/plugin')(on);
+//       allureWriter(on, config);
+//       return config;
+//       //screenshotOnRunFailure=true;
+//     },
+//   },
+// });
